13c377c6d6c5418618a321b947376b831630809b
Frank Grießhammer <fgriessh@adobe.com>
2023-08-07 15:19:16 +0200

    allow comparing fonts/UFOs of different UPMs to each other

--- verticalMetricsComparisonProof.py
+++ verticalMetricsComparisonProof.py
@@ -16,36 +16,41 @@ Input: folder containing font or UFO files.
 import defcon
 import drawBot as db
 
-from proofing_helpers import fontSorter
-from proofing_helpers.files import get_font_paths, get_ufo_paths
-from proofing_helpers.globals import FONT_MONO
-
 from verticalMetricsProof import *
 
-GLYPH_NAMES = list('Hnxphlg')
+from proofing_helpers import fontSorter
+from proofing_helpers.files import get_font_paths, get_ufo_paths
+from proofing_helpers.drawing import draw_glyph
+from proofing_helpers.globals import FONT_MONO
 
 
-def draw_metrics_page_ufo(glyph_name, font_list, page_width=1000):
-    scale_factor = PT_SIZE / 1000
+EXAMPLE_CHARS = list('Hnxphlg')
+
+
+def draw_metrics_page_ufo(character, fo_list, cmap_list, page_width=1000):
+
     db.newPage(page_width, 250)
-    x_offset = MARGIN / scale_factor
-    baseline = db.height() / 3 / scale_factor
+    x_offset = MARGIN
 
-    for font in font_list:
+    for i, font in enumerate(fo_list):
+        scale_factor = PT_SIZE / font.info.unitsPerEm
+        baseline = db.height() / 3 / scale_factor
         line_y = (
-            font.info.descender,
+            font.info.descender if font.info.descender else -250,
             0,
-            font.info.xHeight,
-            font.info.capHeight,
-            font.info.ascender
+            font.info.xHeight if font.info.xHeight else 500,
+            font.info.capHeight if font.info.capHeight else 750,
+            font.info.ascender if font.info.ascender else 750
         )
+        char_map = cmap_list[i]
+        glyph_name = char_map.get(ord(character))
         with db.savedState():
             db.scale(scale_factor)
-            db.translate(x_offset, baseline)
+            db.translate(x_offset / scale_factor, baseline)
             db.stroke(None)
             glyph = font[glyph_name]
             draw_glyph(glyph)
-            x_offset += glyph.width
+            x_offset += glyph.width * scale_factor
             with db.savedState():
                 for y_value in line_y:
                     db.stroke(0)
@@ -54,27 +59,28 @@ def draw_metrics_page_ufo(glyph_name, font_list, page_width=1000):
             with db.savedState():
                 for y_value in [v for v in line_y if v != 0]:
                     db.font(FONT_MONO)
-                    db.fontSize(30)
+                    db.fontSize(6 / scale_factor)
                     # db.fill(0, 0.981, 0.574)  # Sea Foam
                     db.fill(1, 0.186, 0.573)  # Strawberry
                     db.text(
                         str(y_value),
-                        (glyph.width / 2, y_value + 10),
+                        (glyph.width / 2, y_value + 2 / scale_factor),
                         align='center')
                 db.text(
                     font.info.styleName,
-                    (glyph.width / 2, - baseline + 20),
+                    (glyph.width / 2, -baseline + 4 / scale_factor),
                     align='center')
 
 
-def draw_metrics_page_font(glyph_name, font_info_list, page_width=1000):
+def draw_metrics_page_font(character, font_info_list, page_width=1000):
+
     db.newPage(page_width, 250)
-    upm = font_info_list[0].upm
-    scale_factor = PT_SIZE / upm
-    x_offset = MARGIN / scale_factor
-    baseline = db.height() / 3 / scale_factor
+    x_offset = MARGIN
 
     for f_info in font_info_list:
+        upm = f_info.upm
+        scale_factor = PT_SIZE / upm
+        baseline = db.height() / 3 / scale_factor
 
         line_y = (
             f_info.descender,
@@ -83,18 +89,18 @@ def draw_metrics_page_font(glyph_name, font_info_list, page_width=1000):
             f_info.capHeight,
             f_info.ascender
         )
-        character = f_info.reverse_char_map.get(glyph_name)
+        glyph_name = f_info.char_map.get(ord(character))
         with db.savedState():
             db.font(f_info.path)
             db.fontSize(f_info.upm)
             db.scale(scale_factor)
-            db.translate(x_offset, baseline)
+            db.translate(x_offset / scale_factor, baseline)
             db.text(
                 character,
                 (0, 0)
             )
             glyph_width = f_info.advance_widths[glyph_name]
-            x_offset += glyph_width
+            x_offset += glyph_width * scale_factor
             with db.savedState():
                 for y_value in line_y:
                     db.stroke(0)
@@ -103,16 +109,16 @@ def draw_metrics_page_font(glyph_name, font_info_list, page_width=1000):
             with db.savedState():
                 for y_value in [v for v in line_y if v != 0]:
                     db.font(FONT_MONO)
-                    db.fontSize(30)
+                    db.fontSize(6 / scale_factor)
                     # db.fill(0, 0.981, 0.574)  # Sea Foam
                     db.fill(1, 0.186, 0.573)  # Strawberry
                     db.text(
                         str(y_value),
-                        (glyph_width / 2, y_value + 10),
+                        (glyph_width / 2, y_value + 2 / scale_factor),
                         align='center')
                 db.text(
                     f_info.styleName,
-                    (glyph_width / 2, - baseline + 20),
+                    (glyph_width / 2, - baseline + 4 / scale_factor),
                     align='center')
 
 
@@ -138,8 +144,8 @@ def process_font_paths(font_paths, args):
             f_info.capHeight,
             f_info.ascender))
 
-    for g_name in GLYPH_NAMES:
-        draw_metrics_page_font(g_name, font_info_list, page_width)
+    for char in EXAMPLE_CHARS:
+        draw_metrics_page_font(char, font_info_list, page_width)
 
     finish_drawing(doc_name)
 
@@ -147,25 +153,39 @@ def process_font_paths(font_paths, args):
 def process_ufo_paths(ufo_paths, args):
     font_list = fontSorter.sort_fonts(ufo_paths)
     fo_list = [defcon.Font(f) for f in font_list]
+    upm_list = [f.info.unitsPerEm for f in fo_list]
+    cmap_list = [{g.unicode: g.name for g in f if g.unicode} for f in fo_list]
+    gnames_H = [cmap.get(ord('H')) for cmap in cmap_list]
+
     family_name = fo_list[0].info.familyName
     if args.output_file_name:
         doc_name = f'comparison {args.output_file_name}'
     else:
         doc_name = f'comparison {family_name} (UFO)'
+    # get combined width of Hs – no matter which glyph name or UPM they have
     page_width = sum(
-        [fo['H'].width * PT_SIZE / 1000 for fo in fo_list]
+        [fo[gnames_H[i]].width * PT_SIZE / upm_list[i] for i, fo in enumerate(fo_list)]
     ) + 2 * MARGIN
 
     for fo in fo_list:
-        print('{:20s} {:>3d} 0 {:>3d} {:>3d} {:>3d}'.format(
-            fo.info.styleName,
-            fo.info.descender,
-            fo.info.xHeight,
-            fo.info.capHeight,
-            fo.info.ascender))
+        # terminal feedback
+        format_dict = {
+            'styleName': fo.info.styleName,
+            'descender': fo.info.descender if fo.info.descender else 0,
+            'xHeight': fo.info.xHeight if fo.info.xHeight else 0,
+            'capHeight': fo.info.capHeight if fo.info.capHeight else 0,
+            'ascender': fo.info.ascender if fo.info.ascender else 0,
+        }
 
-    for g_name in GLYPH_NAMES:
-        draw_metrics_page_ufo(g_name, fo_list, page_width)
+        print('{:20s} {:>3d} 0 {:>3d} {:>3d} {:>3d}'.format(
+            format_dict.get('styleName'),
+            format_dict.get('descender'),
+            format_dict.get('xHeight'),
+            format_dict.get('capHeight'),
+            format_dict.get('ascender')))
+
+    for char in EXAMPLE_CHARS:
+        draw_metrics_page_ufo(char, fo_list, cmap_list, page_width)
 
     finish_drawing(doc_name)
 
